1bbdf6edc496cd548f3115e96b2efb011c9288d7
Marc SAHLER <MCSA@bachmann.at>
2023-11-24 16:06:54 +0100

    Deleted VibezDbContext and put the DbSet in the old ApplicationContext

--- Vibez/Vibez/Data/ApplicationDbContext.cs
+++ Vibez/Vibez/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Vibez.Data.Models;
 
 namespace Vibez.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
+        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public virtual DbSet<Event> Events { get; set; }
     }
 }
