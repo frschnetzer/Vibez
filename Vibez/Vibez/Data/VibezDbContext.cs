8dd16aef9fed9c4ba90224963b6e742e8125788b
Marc SAHLER <MCSA@bachmann.at>
2023-11-24 15:56:06 +0100

    Created VibezDbContext

---
+++ Vibez/Vibez/Data/VibezDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vibez.Data.Models;
+
+namespace Vibez.Data
+{
+    public class VibezDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=VibezDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public virtual DbSet<Event> Events { get; set; }
+    }
+}
+
