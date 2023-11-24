163712ea88271cdf6165f99c416b42e045181fcc
Marc SAHLER <MCSA@bachmann.at>
2023-11-24 15:19:05 +0100

    Created Model Folder Created ApplicationUser Model and Event Model

---
+++ Vibez/Vibez/Data/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vibez.Data.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+
+        [Required]
+        [StringLength(50)]
+        public string UserNickname { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(6)]
+        public string UserNicknameIdentifier { get; set; } = string.Empty;
+    }
+}
---
+++ Vibez/Vibez/Data/Models/Event.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Azure.Core.HttpHeader;
+
+namespace Vibez.Data.Models
+{
+    public class Event
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string EventName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string CreatorName { get; set; } = string.Empty;
+
+        public int ParticipantCount { get; set; }
+
+        [Required]
+        public string LocationName { get; set; } = string.Empty;
+
+        [Required]
+        public double CordinatesLongitude { get; set; }
+
+        [Required]
+        public double CordinatesLatitude { get; set; }
+
+        [StringLength(500)]
+        public string Notes { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
