872aefd8372af785c44c242bf9000deda63836a7
Moneyboy <96113821+Marc-559@users.noreply.github.com>
2023-11-27 14:42:13 +0100

    Created VibezDbContext (#25)
    * Created VibezDbContext
    
    * Deleted VibezDbContext and put the DbSet in the old ApplicationContext
    
    * Added a Relation betwenn the two models and changed the Context accordingly
    
    ---------
    
    Co-authored-by: Marc SAHLER <MCSA@bachmann.at>

--- Vibez/Vibez/Data/ApplicationDbContext.cs
+++ Vibez/Vibez/Data/ApplicationDbContext.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Vibez.Data.Models;
 
 namespace Vibez.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>()
+                .HasMany(c => c.ApplicationUsers)
+                .WithMany(C => C.Events)
+                .UsingEntity(p => p.ToTable("ApplicationUserEvents"));
+        }
+           
+        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public virtual DbSet<Event> Events { get; set; }
     }
 }
