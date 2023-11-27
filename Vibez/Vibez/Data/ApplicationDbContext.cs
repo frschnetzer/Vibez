d1d1395f527478a3dffe028c8396e75734659deb
Marc SAHLER <MCSA@bachmann.at>
2023-11-27 14:39:35 +0100

    Added a Relation betwenn the two models and changed the Context accordingly

--- Vibez/Vibez/Data/ApplicationDbContext.cs
+++ Vibez/Vibez/Data/ApplicationDbContext.cs
@@ -8,8 +8,17 @@ namespace Vibez.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>()
+                .HasMany(c => c.ApplicationUsers)
+                .WithMany(C => C.Events)
+                .UsingEntity(p => p.ToTable("ApplicationUserEvents"));
+        }
+           
         public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public virtual DbSet<Event> Events { get; set; }
     }
