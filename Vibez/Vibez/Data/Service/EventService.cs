444861cbe6dd1aa6cfad9b418acc8df61b8d8dc5
Schnetzer Franziska <franziska.schnetzer@liebherr.com>
2023-12-15 13:02:02 +0100

    rebasing

--- Vibez/Vibez/Data/Service/EventService.cs
+++ Vibez/Vibez/Data/Service/EventService.cs
@@ -7,33 +7,24 @@ namespace Vibez.Data.Service
     {
         ApplicationDbContext context;
 
-        public EventService(ApplicationDbContext context)
-        {
-            this.context = context;
-        }
+        public EventService(ApplicationDbContext context) => this.context = context;
 
         public async Task AddEvent(Event newEvent)
         {
-            try
-            {
-                if (await EventIsValid(newEvent.Id))
-                {
+            try {
+                if (await EventIsValid(newEvent.Id)) {
                     await context.Events.AddAsync(newEvent);
                     await context.SaveChangesAsync();
-                }        
+                }
+            } catch (Exception ex) {
+                throw new Exception($"Couldn't add event to database. See following exception: {ex}");
             }
-            catch (Exception ex)
-            {
-                throw new Exception ($"Couldn't add event to database. See following exception: {ex}"  );
-            }  
         }
 
         public async Task UpdateEvent(Event newEvent)
         {
-            try
-            {
-                if (await EventIsValid(newEvent.Id))
-                {
+            try {
+                if (await EventIsValid(newEvent.Id)) {
                     var oldEvent = await context.Events.FirstAsync(x => x.Id == newEvent.Id);
 
                     oldEvent.EventName = newEvent.EventName;
@@ -45,27 +36,22 @@ namespace Vibez.Data.Service
                     oldEvent.CordinatesLatitude = newEvent.CordinatesLatitude;
                     oldEvent.CordinatesLongitude = newEvent.CordinatesLongitude;
                     oldEvent.ApplicationUsers = newEvent.ApplicationUsers;
-                    
+
                     await context.SaveChangesAsync();
                 }
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 throw new Exception($"Couldn't update event. See following exception: {ex}");
             }
         }
 
         public async Task DeleteEventById(int eventId)
         {
-            try
-            {
+            try {
                 var deleteEvent = await context.Events.FirstAsync(x => x.Id == eventId);
 
                 context.Events.Remove(deleteEvent);
                 await context.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 throw new Exception($"Couldn't delete event. See following exception: {ex}");
             }
         }
@@ -74,8 +60,7 @@ namespace Vibez.Data.Service
         {
             bool isDuplicate = await context.Events.AnyAsync(x => x.Id == eventId);
 
-            if (!isDuplicate)
-            {
+            if (!isDuplicate) {
                 return false;
             }
             return true;
