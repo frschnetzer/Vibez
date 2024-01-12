cb95f4e059b586022f2eed72a49fd4080d088f39
Moneyboy <96113821+Marc-559@users.noreply.github.com>
2024-01-12 15:48:24 +0100

    Reworked Services, changed Event Object and changed EventDTO (#61)
    * Reworked Services, changed Event Object and changed EventDTO
    
    * added spacebar
    
    ---------
    
    Co-authored-by: Marc SAHLER <MCSA@bachmann.at>

--- Vibez/Vibez/Data/Service/IFriendService.cs
+++ Vibez/Vibez/Data/Service/IFriendService.cs
@@ -6,6 +6,7 @@ namespace Vibez.Data.Service
     public interface IFriendService
     {
         Task AddFriend(IdentityUser user);
+        Task<List<Friend>> GetAllFriendsByUser(IdentityUser user);
         Task<Friend> GetFriendByUser(IdentityUser user);
         Task RemoveFriend(Friend friend);
     }
