85ccd99671e0cf438862a8c704c8d72bb6971a55
Marc SAHLER <MCSA@bachmann.at>
2024-01-12 15:41:13 +0100

    Reworked Services, changed Event Object and changed EventDTO

--- Vibez/Vibez/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Vibez/Vibez/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -248,12 +248,6 @@ namespace Vibez.Data.Migrations
                     b.Property<string>("ApplicationUserId")
                         .HasColumnType("nvarchar(450)");
 
-                    b.Property<double>("CordinatesLatitude")
-                        .HasColumnType("float");
-
-                    b.Property<double>("CordinatesLongitude")
-                        .HasColumnType("float");
-
                     b.Property<string>("CreatorName")
                         .IsRequired()
                         .HasMaxLength(50)
@@ -266,6 +260,10 @@ namespace Vibez.Data.Migrations
                         .IsRequired()
                         .HasMaxLength(100)
                         .HasColumnType("nvarchar(100)");
+
+                    b.Property<string>("EventTime")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("LocationName")
                         .IsRequired()
