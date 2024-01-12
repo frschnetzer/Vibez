fe1bd4565a005905a36c4d1f56ed4c9133c7f7be
Marc SAHLER <MCSA@bachmann.at>
2024-01-12 13:32:06 +0100

    Added Friend Table

--- Vibez/Vibez/Data/Migrations/20240112123014_AddFriends.cs
+++ Vibez/Vibez/Data/Migrations/20240112123014_AddFriends.cs
@@ -5,45 +5,43 @@
 namespace Vibez.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class AddFriend : Migration
+    public partial class AddFriends : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Friend",
+                name: "Friends",
                 columns: table => new
                 {
                     FriendId = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    IdentityUserId = table.Column<int>(type: "int", nullable: false),
-                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
                     ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Friend", x => x.FriendId);
+                    table.PrimaryKey("PK_Friends", x => x.FriendId);
                     table.ForeignKey(
-                        name: "FK_Friend_AspNetUsers_ApplicationUserId",
+                        name: "FK_Friends_AspNetUsers_ApplicationUserId",
                         column: x => x.ApplicationUserId,
                         principalTable: "AspNetUsers",
                         principalColumn: "Id");
                     table.ForeignKey(
-                        name: "FK_Friend_AspNetUsers_UserId",
+                        name: "FK_Friends_AspNetUsers_UserId",
                         column: x => x.UserId,
                         principalTable: "AspNetUsers",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
+                        principalColumn: "Id");
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_Friend_ApplicationUserId",
-                table: "Friend",
+                name: "IX_Friends_ApplicationUserId",
+                table: "Friends",
                 column: "ApplicationUserId");
 
             migrationBuilder.CreateIndex(
-                name: "IX_Friend_UserId",
-                table: "Friend",
+                name: "IX_Friends_UserId",
+                table: "Friends",
                 column: "UserId");
         }
 
@@ -51,7 +49,7 @@ namespace Vibez.Data.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Friend");
+                name: "Friends");
         }
     }
 }
