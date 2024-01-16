b90ff4d8eacb34170a4ea7d0854373fedf1bd76a
Moneyboy <96113821+Marc-559@users.noreply.github.com>
2024-01-16 15:44:21 +0100

    62 add event properties (#64)
    * Changed Event Props and EventDTO Props
    
    * Changed typo and getAllPastEvents update changed
    
    * Added migriation to Update database
    
    ---------
    
    Co-authored-by: Marc SAHLER <MCSA@bachmann.at>

---
+++ Vibez/Vibez/Data/Migrations/20240116143943_ChangedEventAddressName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vibez.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedEventAddressName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Adress",
+                table: "Events",
+                newName: "Address");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Events",
+                newName: "Adress");
+        }
+    }
+}
